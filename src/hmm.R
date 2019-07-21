8389239769fd97813f82363c0abfa2aebad92c73
ZackLarsen <zlarsen4@gmail.com>
2019-07-21 10:25:56 -0500

    Add file to src

---
+++ src/hmm.R
@@ -0,0 +1,12 @@
+##################################################
+## Project: Hidden Markov Models
+## Script purpose: Create HMM for POS tagging example
+## Date: July 21, 2019
+## Author: Zack Larsen
+##################################################
+
+library(pacman)
+library(tidyverse)
+library(magrittr)
+
+p_load(HMM, TraMineR)
