241d3637f7360cc20dff042f28a072da9a7f0e84
ZackLarsen <zlarsen4@gmail.com>
2019-08-11 15:22:52 -0500

    Successfully run Janet example

--- src/janet_pomegranate.py
+++ src/janet_pomegranate.py
@@ -65,29 +65,84 @@ model.add_transition(model.start, s5, 0.0449)
 model.add_transition(model.start, s6, 0.051)
 model.add_transition(model.start, s7, 0.2026)
 
-# Transitions
-model.add_transition(s1, s1, 0.80)
-model.add_transition(s1, s2, 0.20)
-model.add_transition(s2, s2, 0.90)
-model.add_transition(s2, s3, 0.10)
-model.add_transition(s3, s3, 0.70)
 
-model.add_transition(s3, model.end, 0.30)
+# Transitions
+model.add_transition(s1, s1, 0.3777)
+model.add_transition(s1, s2, 0.0110)
+model.add_transition(s1, s3, 0.0009)
+model.add_transition(s1, s4, 0.0084)
+model.add_transition(s1, s5, 0.0584)
+model.add_transition(s1, s6, 0.0090)
+model.add_transition(s1, s7, 0.0025)
+
+model.add_transition(s2, s1, 0.0008)
+model.add_transition(s2, s2, 0.0002)
+model.add_transition(s2, s3, 0.7968)
+model.add_transition(s2, s4, 0.0005)
+model.add_transition(s2, s5, 0.0008)
+model.add_transition(s2, s6, 0.1698)
+model.add_transition(s2, s7, 0.0041)
+
+model.add_transition(s3, s1, 0.0322)
+model.add_transition(s3, s2, 0.0005)
+model.add_transition(s3, s3, 0.0050)
+model.add_transition(s3, s4, 0.0837)
+model.add_transition(s3, s5, 0.0615)
+model.add_transition(s3, s6, 0.0514)
+model.add_transition(s3, s7, 0.2231)
+
+model.add_transition(s4, s1, 0.0366)
+model.add_transition(s4, s2, 0.0004)
+model.add_transition(s4, s3, 0.0001)
+model.add_transition(s4, s4, 0.0733)
+model.add_transition(s4, s5, 0.4509)
+model.add_transition(s4, s6, 0.0036)
+model.add_transition(s4, s7, 0.0036)
+
+model.add_transition(s5, s1, 0.0096)
+model.add_transition(s5, s2, 0.0176)
+model.add_transition(s5, s3, 0.0014)
+model.add_transition(s5, s4, 0.0086)
+model.add_transition(s5, s5, 0.1216)
+model.add_transition(s5, s6, 0.0177)
+model.add_transition(s5, s7, 0.0068)
+
+model.add_transition(s6, s1, 0.0068)
+model.add_transition(s6, s2, 0.0102)
+model.add_transition(s6, s3, 0.1011)
+model.add_transition(s6, s4, 0.1012)
+model.add_transition(s6, s5, 0.0120)
+model.add_transition(s6, s6, 0.0728)
+model.add_transition(s6, s7, 0.0479)
+
+model.add_transition(s7, s1, 0.1147)
+model.add_transition(s7, s2, 0.0021)
+model.add_transition(s7, s3, 0.0002)
+model.add_transition(s7, s4, 0.2157)
+model.add_transition(s7, s5, 0.4744)
+model.add_transition(s7, s6, 0.0102)
+model.add_transition(s7, s7, 0.0017)
+
+model.add_transition(s1, model.end, 0.1)
+model.add_transition(s2, model.end, 0.2)
+model.add_transition(s3, model.end, 0.1)
+model.add_transition(s3, model.end, 0.05)
+model.add_transition(s5, model.end, 0.05)
+model.add_transition(s6, model.end, 0.15)
+model.add_transition(s7, model.end, 0.35)
 
 model.bake()
 
 model
 
-print(model.log_probability(list('Janet','will','back','the','bill')))
+print(model.log_probability(['Janet','will','back','the','bill']))
 
-print(", ".join(state.name for i, state in model.viterbi(list('ACGACTATTCGAT'))[1]))
-
-
-
-
-
-
-
+print(", ".join(state.name for i, state in model.viterbi(['Janet','will','back','the','bill'])[1]))
+# NNP
+# MD
+# VB
+# DT
+# NN
 
 
 
@@ -97,17 +152,14 @@ print(", ".join(state.name for i, state in model.viterbi(list('ACGACTATTCGAT'))[
 
 
 
-dists = [NormalDistribution(5, 1), NormalDistribution(1, 7), NormalDistribution(8,2)]
-trans_mat = transitions
-starts = Pi
-model = HiddenMarkovModel.from_matrix(trans_mat, starts)
-
-
-model
 
 
 
 
 
-
+# dists = [NormalDistribution(5, 1), NormalDistribution(1, 7), NormalDistribution(8,2)]
+# trans_mat = transitions
+# starts = Pi
+# model = HiddenMarkovModel.from_matrix(trans_mat, starts)
+# model
 
