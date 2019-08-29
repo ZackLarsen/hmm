1df08b30a7c3003ee6ee9b8a180e86eb033872e6
ZackLarsen <zlarsen4@gmail.com>
2019-08-29 13:24:02 -0500

    Update hmm.py

--- src/hmm.py
+++ src/hmm.py
@@ -267,3 +267,48 @@ def viterbi_part2(viterbi_trellis, backpointer):
         states back in time'''
 
     return bestpath, bestpathprob
+
+def hcc_viterbi(observations, transitions_matrix, emissions_matrix, Pi):
+    """
+    Compute the optimal sequence of hidden states
+    Note that -inf (or np.NINF) for negative infinity is being used as a default
+    value to represent zero probabilities because we are computing in log10 space
+
+    :param observations:
+    :param transitions_matrix:
+    :param emissions_matrix:
+    :param Pi:
+    :return:
+    """
+
+    # Initialization
+    N = emissions_matrix.shape[0] # Number of states (hidden states)
+    T = len(observations) # Number of observations (observations)
+    viterbi_trellis = np.ones((N, T)) * np.NINF
+    backpointer = np.zeros_like(viterbi_trellis)
+    for s in range(0, N):
+        viterbi_trellis[s, 0] = Pi[s] + emissions_matrix[s, observations[0]]
+
+    # Recursion
+    for time_step in range(1, T):
+        for current_state in range(0, N):
+            priors = np.zeros((N, 2))
+            for previous_state in range(0, N):
+                priors[previous_state, 0] = previous_state
+                priors[previous_state, 1] = viterbi_trellis[previous_state, time_step - 1] + \
+                                            transitions_matrix[previous_state, current_state] + \
+                                            emissions_matrix[current_state, observations[time_step]] # Previously, we were using timestep-1 here
+            viterbi_trellis[current_state, time_step] = np.amax(priors[:, 1], axis=0)
+            backpointer[current_state, time_step] = np.argmax(priors[:, 1], axis=0)
+
+    # Termination
+    bestpathprob = np.amax(viterbi_trellis[:, -1])
+    bestpathpointer = np.argmax(viterbi_trellis[:, -1])
+    viterbi_cell_idx = np.argmax(viterbi_trellis, axis=0)
+    viterbi_hidden_states = []
+    for i, ix in enumerate(viterbi_cell_idx):
+        viterbi_hidden_states.append(backpointer[ix, i])
+    viterbi_hidden_states = np.delete(viterbi_hidden_states, 0)  # We don't need the first entry - it is always zero
+    viterbi_hidden_states = np.append(viterbi_hidden_states, bestpathpointer) # Add the bestpathpointer to the last entry
+
+    return bestpathprob, viterbi_hidden_states
